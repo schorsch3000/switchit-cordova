08dd2a38882d8e3a95d168489a78a29e59c6505d
Dirk Heilig <dicky.dicky@gmail.com>
2015-10-03 14:56:56 +0200

    fix wired apk naming thingy

--- makerelease.sh
+++ makerelease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cordova build android --release
-cp platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk .
+cp platforms/android/build/outputs/apk/android-release-unsigned.apk .
 jarsigner -keypass password -storepass password -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/my-release-key.keystore android-armv7-release-unsigned.apk  alias_name
 zipalign -f 4 android-armv7-release-unsigned.apk out.apk
