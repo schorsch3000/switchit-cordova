b301d5a0f6dd69019927dc7793cef5137ae26377
Dirk Heilig <dicky.dicky@gmail.com>
2015-10-03 17:46:47 +0200

    it runs!

--- makerelease.sh
+++ makerelease.sh
@@ -10,9 +10,9 @@ git commit androidVersion -m "bump android version to $NEXT_ANDROID_VERSION"
 git push
 git checkout $BRANCH
 echo $NEXT_ANDROID_VERSION >androidVersion
-xsltproc --stringparam version $NEXT_ANDROID_VERSION setAndroidVersion.xsl config.xml
+mv config.xml config.xml.old
+xsltproc --stringparam version $NEXT_ANDROID_VERSION setAndroidVersion.xsl config.xml.old > config.xml
 
-false
 cordova build android --release
 cp platforms/android/build/outputs/apk/android-release-unsigned.apk .
 jarsigner -keypass password -storepass password -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/my-release-key.keystore android-release-unsigned.apk  alias_name
