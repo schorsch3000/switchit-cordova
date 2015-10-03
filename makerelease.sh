bed20f493ab4daa99fc2fb86abfe9f9c8bd9760a
Dirk Heilig <dicky.dicky@gmail.com>
2015-10-03 17:47:44 +0200

    added init since we switched branches

--- makerelease.sh
+++ makerelease.sh
@@ -12,7 +12,7 @@ git checkout $BRANCH
 echo $NEXT_ANDROID_VERSION >androidVersion
 mv config.xml config.xml.old
 xsltproc --stringparam version $NEXT_ANDROID_VERSION setAndroidVersion.xsl config.xml.old > config.xml
-
+./init.sh
 cordova build android --release
 cp platforms/android/build/outputs/apk/android-release-unsigned.apk .
 jarsigner -keypass password -storepass password -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/my-release-key.keystore android-release-unsigned.apk  alias_name
